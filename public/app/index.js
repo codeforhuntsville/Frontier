907eb412a730f0440e393cbbe4e862ec536addb0
Chad McElligott <cmcelligott@digium.com>
2015-05-06 21:49:50 -0500

    Basic functionality for retrieving local info
    Spike of some basic functionality for retrieving local restaurant
    information from the google places api.
    
    Pairing session with Madhav Gentela.

---
+++ public/app/index.js
@@ -0,0 +1,39 @@
+;(function () {
+  /* global fetch: false */
+  'use strict';
+
+  function getRestaurants(coords) {
+    var fetchUrl = '/restaurants?latitude=' + coords.latitude + '&longitude=' + coords.longitude;
+    return fetch(fetchUrl).then(function (response) {
+      if (response.statusCode < 200 || response.statusCode >= 300) {
+        throw new Error(response.statusText);
+      }
+
+      return response.json();
+    });
+  }
+
+  var $status = document.getElementById('status');
+
+  if (!navigator.geolocation) {
+    $status.innerHTML = 'gelocation is not supported on this platform';
+  }
+
+  $status.innerHTML = 'retrieving your location...';
+  navigator.geolocation.getCurrentPosition(function (position) {
+    $status.innerHTML = 'your location is: lat(' + position.coords.latitude + '), lon(' + position.coords.longitude + ')';
+    console.log('Geolocation position', position);
+
+    getRestaurants(position.coords).then(function (restaurants) {
+      $status.innerHTML = restaurants.map(function (r) {
+        return r.name + '<br />';
+      }).join('');
+    }).catch(function (err) {
+      $status.innerHTML = 'Error retrieving restaurants<br />' + err;
+    });
+
+  }, function (error) {
+    $status.innerHTML = 'location could not be retrieved.';
+    console.error('Geolocation error', error);
+  });
+})();
