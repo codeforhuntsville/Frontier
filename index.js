972b18902ff9e9a1df6fa2d5b9e83f90e79d1e38
Chad McElligott <cmcelligott@digium.com>
2015-05-07 13:51:46 -0500

    Make ngrok an optional dependency
    This patch makes ngrok an optional dependency, so that users that
    are behind firewalls that disallow the ngrok url can still complete
    their npm install. If the ngrok install is successful, an ngrok
    url will be provided upon server start.

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ var server = require('./lib/server')();
 var config = require('config');
 var port = config.get('port');
 
+function ngrokIsAvailable() {
+  try {
+    require.resolve('ngrok');
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
 server.listen(port, function () {
   console.log('listening on port ' + port);
 
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === 'development' && ngrokIsAvailable()) {
     require('ngrok').connect(port, function (err, url) {
       if (err) {
         console.error('ngrok error', err);
