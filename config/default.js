596219d5f95c84ae85608884ffb64eedc3c2b7fd
Chris Heath <nedlinin@gmail.com>
2015-05-20 14:22:06 -0500

    Added a bit of configuration checking in the default config.  Adding it into default allows us to easily override the checks for production or development builds if desired (though I'm not sure we would ever actually *want* that..)
    Right now the only check is for whether or not the Google Places API key appears to be supplied. We don't even check if the key is valid just that the parameter was supplied.
    
    Removed the now unneccessary check of whether the key was supplied from the restaurants end point.

--- config/default.js
+++ config/default.js
@@ -1,5 +1,36 @@
 'use strict';
 module.exports = {
   port: 8080,
-  googlePlacesApiKey: '' // define this in local.js
+  googlePlacesApiKey: '', // define this in local.js
+
+  /**
+   * Shortcut function to get only the isValid property from config validity report.
+   * @returns {boolean}
+   */
+  isValid: function() {
+    return this.getConfigValidityReport().isValid;
+  },
+
+  /**
+   * Generates a "report" on whether or not the config is valid.  Report contains all error messages
+   * as well as a simple "isValid" property.
+   */
+  getConfigValidityReport: function() {
+    // Assume zero errors and valid by default.
+    var summary = { errors: [], isValid: true };
+    // Check the configuration parameters.  Return false on invalid config.
+    if (!this.googlePlacesApiKey) {
+      summary.errors.push("Places API key was not defined.  Please supply a Google Places API Key and try again.  Refer to README file for more information.");
+    }
+
+    // TODO: Add any additional checks here.
+
+    // Set the isValid flag to false if errors present.
+    if (summary.errors.length !== 0) {
+      summary.isValid = false;
+    }
+
+    // All checks completed successfully.  Config appears valid!
+    return summary;
+  }
 };
