4840f0296869dc9e78856cf577d766e3ce3ccabe
Chris Heath <nedlinin@gmail.com>
2015-05-20 14:44:17 -0500

    Rearrange code to reduce nesting as requested by PR comment.

--- index.js
+++ index.js
@@ -11,28 +11,31 @@ function ngrokIsAvailable() {
   }
 }
 
-if (config.isValid()) {
-  var port = config.get("port") || 8080;
-  server.listen(port, function () {
-    console.log('listening on port ' + port);
-
-    if (process.env.NODE_ENV === 'development' && ngrokIsAvailable()) {
-      require('ngrok').connect(port, function (err, url) {
-        if (err) {
-          console.error('ngrok error', err);
-          return;
-        }
-
-        console.log('publicly accessible https url is: ' + url);
-      });
-    }
-  });
-}
-else {
+/**
+ * Make sure to configuration is valid before attempting to start the server.
+ */
+if (!config.isValid()) {
   config.getConfigValidityReport().errors.forEach(function(error){
     console.log(error);
   });
   console.error("\n\nInvalid configuration detected.  Aborted server startup.\n");
   return;
 }
+
+var port = config.get("port") || 8080;
+server.listen(port, function () {
+  console.log('listening on port ' + port);
+
+  if (process.env.NODE_ENV === 'development' && ngrokIsAvailable()) {
+    require('ngrok').connect(port, function (err, url) {
+      if (err) {
+        console.error('ngrok error', err);
+        return;
+      }
+
+      console.log('publicly accessible https url is: ' + url);
+    });
+  }
+});
+
 
